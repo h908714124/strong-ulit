cc9a63431ca90a656c996481459321ba776cb36e
h908714124 <kraftdurchblumen@gmx.de>
2019-10-31 13:06:10 +0100

    bulk mode

--- src/main/java/com/strong/ulit/RemoveSingleLinebreaks.java
+++ src/main/java/com/strong/ulit/RemoveSingleLinebreaks.java
@@ -11,9 +11,12 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @CommandLineArguments
 abstract class RemoveSingleLinebreaks {
@@ -25,13 +28,29 @@ abstract class RemoveSingleLinebreaks {
   @PositionalParameter(position = 1)
   abstract Path input();
 
-  @PositionalParameter(position = 2)
-  abstract Optional<Path> output();
-
   static void run(RemoveSingleLinebreaks args) throws IOException {
-    Path output = args.output().orElse(args.input().resolveSibling(args.input().getFileName() + "_output.txt"));
+    Path input = args.input();
+    Charset charset = args.charset().orElse(StandardCharsets.UTF_8);
+    if (input.toFile().isDirectory()) {
+      Path outputDir = input.resolveSibling(input.getFileName() + "_output");
+      outputDir.toFile().mkdirs();
+      try (Stream<Path> pathStream = Files.find(input, Integer.MAX_VALUE, (path, attr) -> attr.isRegularFile())) {
+        List<Path> inputs = pathStream
+            .filter(p -> p.getFileName().toString().endsWith(".txt"))
+            .collect(Collectors.toList());
+        for (Path file : inputs) {
+          doFile(charset, file, outputDir.resolve(file.getFileName()));
+        }
+      }
+    } else {
+      doFile(charset, input, input.resolveSibling(input.getFileName() + "_output.txt"));
+    }
+  }
+
+  private static void doFile(Charset charset, Path input, Path output) throws IOException {
     output.toFile().delete();
-    try (BufferedReader in = Files.newBufferedReader(args.input(), args.charset().orElse(StandardCharsets.UTF_8));
+    output.getParent().toFile().mkdirs();
+    try (BufferedReader in = Files.newBufferedReader(input, charset);
          PrintWriter out = new PrintWriter(Files.newBufferedWriter(output))) {
       String line;
       StringBuilder sb = new StringBuilder();
